538e26176574122b6d96cd3ffaf368ccc7e62ed1
skin93 <arro.ciesielski@gmail.com>
2019-05-17 20:44:59 +0200

    Customer / MovieForm added

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using Vidly.Migrations;
 using Vidly.Models;
 using Vidly.ViewModels;
 
@@ -10,60 +12,115 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
 
         // GET: Movies
         public ActionResult Index()
         {
-            IEnumerable<Movie> movies = GetMovies();
+            var movies = _context.Movies.Include(m => m.Genre).ToList();
 
             return View(movies);
         }
 
-        private IEnumerable<Movie> GetMovies()
+        public ViewResult New()
         {
-            return new List<Movie>
+            var genres = _context.Genres.ToList();
+            var viewModel = new MovieFormViewModel
             {
-                new Movie {Id = 1, Name = "Shrek"},
-                new Movie {Id = 2, Name = "Wall-e"}
+                Genres = genres
             };
+            return View("MovieForm", viewModel);
         }
-        // GET: Movies/Random
-        public ActionResult Random()
+
+        [HttpPost]
+        public ActionResult Save(Movie movie)
         {
-            var movie = new Movie() { Name = "Shrek!" };
-            var customers = new List<Customer>
+            if (movie.Id == 0)
             {
-                new Customer {Name = "Customer 1"},
-                new Customer {Name = "Customer 2"}
-            };
-
-            var viewModel = new RandomMovieViewModel
+                movie.DateAdded = System.DateTime.Now;
+                _context.Movies.Add(movie);
+            }
+            else
             {
-                Movie = movie,
-                Customers = customers
-            };
-            
-            return View(viewModel);
+                var movieInDb = _context.Movies.Single(c => c.Id == movie.Id);
 
-            //return Content("Hello World!");
+                //Mapper.Map(movie, movieInDb);
 
-            //return HttpNotFound();
+                movieInDb.Name = movie.Name;
+                movieInDb.ReleaseDate = movie.ReleaseDate;
+                movieInDb.GenreId = movie.GenreId;
+                movieInDb.NumberInStock = movie.NumberInStock;
 
-            //return new EmptyResult();
+            }
+            _context.SaveChanges();
 
-            //return RedirectToAction("Index", "Home");
+            return RedirectToAction("Index", "Movies");
         }
-        [Route("movies/released/{year}/{month:regex(\\d{4}):range(1,12)}")]
-        public ActionResult ByReleaseDate(int year, int month)
+
+        public ActionResult Details(int id)
         {
-            return Content(year + "/" + month);
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
+            if (movie == null)
+                return HttpNotFound();
+
+            return View(movie);
         }
 
         public ActionResult Edit(int id)
         {
-            return Content("id=" + id);
+            var movie = _context.Movies.SingleOrDefault(c => c.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            var viewModel = new MovieFormViewModel
+            {
+                Movie = movie,
+                Genres = _context.Genres.ToList()
+            };
+            return View("MovieForm", viewModel);
         }
 
-        
+        // GET: Movies/Random
+        //public ActionResult Random()
+        //{
+        //    var movie = new Movie() { Name = "Shrek!" };
+        //    var customers = new List<Customer>
+        //    {
+        //        new Customer {Name = "Customer 1"},
+        //        new Customer {Name = "Customer 2"}
+        //    };
+
+        //    var viewModel = new RandomMovieViewModel
+        //    {
+        //        Movie = movie,
+        //        Customers = customers
+        //    };
+
+        //    return View(viewModel);
+
+        //    //return Content("Hello World!");
+
+        //    //return HttpNotFound();
+
+        //    //return new EmptyResult();
+
+        //    //return RedirectToAction("Index", "Home");
+        //}
+        //[Route("movies/released/{year}/{month:regex(\\d{4}):range(1,12)}")]
+        //public ActionResult ByReleaseDate(int year, int month)
+        //{
+        //    return Content(year + "/" + month);
+        //}
     }
 }
--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +10,24 @@ namespace Vidly.Models
     public class Movie
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
+        [ForeignKey("Genre")]
+        public byte GenreId { get; set; }
+        [Required]
+        [Display(Name = "Date Added")]
+        public DateTime DateAdded { get; set; }
+
+        [Display(Name="Release Date")]
+        [Column(TypeName ="datetime2")]
+        public DateTime ReleaseDate { get; set; }
+
+        [Required]
+        [Display(Name = "Number in Stock")]
+        public byte NumberInStock { get; set; }
+        public Genre Genre { get; set; }
+
+
     }
 }
